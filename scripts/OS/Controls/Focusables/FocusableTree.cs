19b696a6faf9708ccaa53e557f1a8a7ba0e5243e
haywirephoenix <andy_reed1@live.co.uk>
2026-05-22 14:08:09 +0100

    move focusables into own folder

--- scripts/OS/Controls/Focusables/FocusableTree.cs
+++ scripts/OS/Controls/Focusables/FocusableTree.cs
@@ -1,9 +1,7 @@
 using Godot;
 public partial class FocusableTree : Tree
 {
-        
     public override void _EnterTree() => FocusManager.Register(this);
 
     public override void _ExitTree() => FocusManager.Unregister(this);
-    
 }
